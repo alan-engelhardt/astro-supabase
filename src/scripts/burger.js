6d57cd950fe8cc70c64fcc06c4233cdb539a59cc
Alan <ale@kea.dk>
2023-03-12 22:42:41 +0100

    src burger.js

---
+++ src/scripts/burger.js
@@ -0,0 +1,7 @@
+const burgerMenu = document.querySelector("#burger-menu");
+const overlay = document.querySelector("#menu");
+
+burgerMenu.addEventListener("click", function () {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
