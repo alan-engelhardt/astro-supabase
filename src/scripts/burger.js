e16a2bbdb0b4648ced4a6b679dd590cc663b9e15
Alan <ale@kea.dk>
2023-03-12 23:35:39 +0100

    inaktiv grå

--- src/scripts/burger.js
+++ src/scripts/burger.js
@@ -1,7 +1,7 @@
 const burgerMenu = document.querySelector("#burger-menu");
-const overlay = document.querySelector("#menu");
+const menu = document.querySelector("#menu");
 
 burgerMenu.addEventListener("click", function () {
   this.classList.toggle("close");
-  overlay.classList.toggle("overlay");
+  menu.classList.toggle("overlay");
 });
